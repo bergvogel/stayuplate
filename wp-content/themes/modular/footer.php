17973d93b35be2b0b6663fbe9d5ce2c246b0fad7
bergvogel <hilbert.kruidenier@gmail.com>
2015-12-10 22:51:01 +0100

    kj

--- wp-content/themes/modular/footer.php
+++ wp-content/themes/modular/footer.php
@@ -8,14 +8,14 @@
  </div>
 
         <div id="social_wrapper">
-          <a href="https://www.facebook.com/ModularMusic/?fref=ts"><img id="social" src="<?bloginfo('template_url');?>/img/social/fb.svg" class="image" /></a>
-          <a href="https://www.instagram.com/modularmusic/"><img id="social" src="<?bloginfo('template_url');?>/img/social/insta.svg" class="image" /></a>
-          <a href="https://soundcloud.com/modular-music"><img id="social" src="<?bloginfo('template_url');?>/img/social/soundcloud.svg" class="image" /></a>
-          <a href="https://www.youtube.com/user/MODULARMUSIC"><img id="social" src="<?bloginfo('template_url');?>/img/social/youtube.svg" class="image" /></a>
+          <a href="https://www.facebook.com/ModularMusic/?fref=ts"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/social/fb.svg" class="image" /></a>
+          <a href="https://www.instagram.com/modularmusic/"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/social/insta.svg" class="image" /></a>
+          <a href="https://soundcloud.com/modular-music"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/social/soundcloud.svg" class="image" /></a>
+          <a href="https://www.youtube.com/user/MODULARMUSIC"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/social/youtube.svg" class="image" /></a>
        </div>
        
    <!-- JS foundation  -->
-<script src="<?bloginfo('template_url');?>/js/foundation.min.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/foundation.min.js"></script>
 <script>$(document).foundation();</script>
 
 
@@ -23,14 +23,14 @@
 
 
    <!-- JS custom  -->
-<script src="<?bloginfo('template_url');?>/js/animations.js" type="text/javascript"></script>
-<script src="<?bloginfo('template_url');?>/js/swiper.js"></script>
-<script src="<?bloginfo('template_url');?>/js/custom_swiper.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/animations.js" type="text/javascript"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/swiper.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/custom_swiper.js"></script>
 
 </div>
 </div>
  <!-- JS smoothstate  -->
-<script src="<?bloginfo('template_url');?>/js/jquery.smoothState.js"></script>
-<script src="<?bloginfo('template_url');?>/js/smooth-functions.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.smoothState.js"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/smooth-functions.js"></script>
 </body>
 </html>
